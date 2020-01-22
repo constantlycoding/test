86d7f001f3e7441a9de0f1b201450c69afbdfa0d
Azmi Kamis <azmi.kamis@constantlycoding.com>
2020-01-22 04:56:14 +0000

    Update mylib.js

--- mylib.js
+++ mylib.js
@@ -2,6 +2,11 @@ var r1 = require.config({
     context: "r1",
     baseUrl: "//raw.githubusercontent.com/dojo/dojo/master/"
 });
-r1(["dojo"], function(dojo) {
-    // ...
+
+r1([
+    'dojo/dom',
+    'dojo/dom-construct'
+], function (dom, domConstruct) {
+    var greetingNode = dom.byId('greeting');
+    domConstruct.place('<em> Dojo!</em>', greetingNode);
 });
